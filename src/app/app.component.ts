165073001c6a5f380b1e1657a4c2081085527a5d
root <wizlif.144@gmail.com>
2017-10-07 08:25:25 +0300

    template 0.01 Teradata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app';
+  routes: Object[] = [{
+    icon: 'home',
+    route: '.',
+    title: 'Home',
+  }, {
+    icon: 'library_books',
+    route: '.',
+    title: 'Documentation',
+  }, {
+    icon: 'color_lens',
+    route: '.',
+    title: 'Style Guide',
+  }, {
+    icon: 'view_quilt',
+    route: '.',
+    title: 'Layouts',
+  }, {
+    icon: 'picture_in_picture',
+    route: '.',
+    title: 'Components & Addons',
+  },
+  ];
+  usermenu: Object[] = [{
+    icon: 'swap_horiz',
+    route: '.',
+    title: 'Switch account',
+  }, {
+    icon: 'tune',
+    route: '.',
+    title: 'Account settings',
+  }, {
+    icon: 'exit_to_app',
+    route: '.',
+    title: 'Sign out',
+  },
+  ];
+  constructor() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {
+  CovalentDialogsModule, CovalentLayoutModule, CovalentMediaModule, CovalentMenuModule,
+  CovalentStepsModule
+} from '@covalent/core';
+import {CovalentHttpModule} from '@covalent/http';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {
+  MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatInputModule, MatListModule,
+  MatMenuModule, MatSidenavModule, MatToolbarModule, MatTooltipModule
+} from '@angular/material';
+import {CommonModule} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatDialogModule,
+    CovalentMenuModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    CovalentMediaModule,
+    CovalentHttpModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
